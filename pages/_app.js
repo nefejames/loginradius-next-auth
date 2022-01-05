89d00198b782d7a3242ba88a9b8401f1a6efb7c1
nefejames <nefejames@yahoo.com>
2022-01-05 22:10:32 +0000

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import { LRAuthProvider } from "loginradius-react";
+import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
+import "../styles/globals.css";
+import Layout from "../layout";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Loginradius Next</title>
+      </Head>
+      <LRAuthProvider
+        appName="nefejames1"
+        apiKey="909cf7d5-5332-4071-ad98-43501a128d82"
+        redirectUri={"http://localhost:3000/"}
+      >
+        <ChakraProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </LRAuthProvider>
+    </>
+  );
+}
+
+export default MyApp;
